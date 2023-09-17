24905a588b509816582a79def0c9ab9cc33dad4e
Sardor Abdullaev <abdullaevss94@mail.ru>
2023-09-17 21:24:27 +0500

    add mongoose connect

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-require('dotenv').config();
-const app = require('./app');
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = require("./app");
+
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log("DB connection successful!"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
