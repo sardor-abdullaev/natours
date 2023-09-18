3a82b93e0028e6c0effdd91fe2bbf7487301d113
Sardor Abdullaev <abdullaevss94@mail.ru>
2023-09-18 07:36:55 +0500

    add tour model

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ mongoose
   .connect(process.env.DATABASE)
   .then(() => console.log("DB connection successful!"));
 
+
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
