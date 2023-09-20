b469c92be37b682740e6a4de146efe7699615168
Sardor <abdullaevss94@mail.ru>
2023-09-20 16:38:25 +0500

    update tour model

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -54,7 +54,7 @@ exports.createTour = async (req, res) => {
   } catch (error) {
     res.status(400).json({
       status: "fail",
-      message: "Invalid data sent",
+      message: error,
     });
   }
 };
