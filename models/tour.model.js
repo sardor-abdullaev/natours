9fa25aece85e868a97d7f82c36f13c13dc5ed082
Sardor Abdullaev <abdullaevss94@mail.ru>
2023-10-08 23:35:25 +0500

    add query middleware

--- models/tour.model.js
+++ models/tour.model.js
@@ -39,6 +39,10 @@ const tourSchema = new mongoose.Schema(
     images: [String],
     createAt: { type: Date, default: Date.now(), select: false },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -66,6 +70,20 @@ tourSchema.pre("save", function (next) {
 //   next();
 // });
 
+// QUERY MIDDLEWARE
+// tourSchema.pre("find", function (next) {
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  // console.log(docs);
+  next();
+});
 const Tour = mongoose.model("Tour", tourSchema);
 
 module.exports = Tour;
