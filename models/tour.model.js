8cbb20180b1b12503591447626edff7fdc8459b7
Sardor Abdullaev <abdullaevss94@mail.ru>
2023-10-11 23:33:15 +0500

    add builtIn validator

--- models/tour.model.js
+++ models/tour.model.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, "A tour must have a name"],
       unique: true,
       trim: true,
+      maxlength: [40, "A tour must have less or equal than 40 characters"],
+      minlength: [10, "A tour must have more or equal than 10 characters"],
     },
     slug: String,
     duration: {
@@ -21,8 +23,17 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty"],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty is either: easy, mediun, difficult",
+      },
     },
-    ratingsAverage: { type: Number, default: 4.5 },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
+    },
     ratingsQuantity: { type: Number, default: 0 },
     price: { type: Number, required: [true, "A tour must have a price"] },
     priceDiscount: Number,
