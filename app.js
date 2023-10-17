31003537db6e6e48db79921aacfb3bbe7694a6b1
Sardor Abdullaev <abdullaevss94@mail.ru>
2023-10-18 00:02:41 +0500

    implement global error

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
-const tourRouter = require('./routes/tour.router');
-const userRouter = require('./routes/user.router');
+const tourRouter = require("./routes/tour.router");
+const userRouter = require("./routes/user.router");
 
 const app = express();
 
 // 1) MIDDLEWARES
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 app.use(express.json());
@@ -20,14 +20,34 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
-app.all("*",(req,res,next)=>{
-  res.status(404).json({
-    status:'fail',
-    message:`Can't find ${req.originalUrl} on this server!`
-  })
-})
+app.all("*", (req, res, next) => {
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = "fail";
+  err.statusCode = 404;
+
+  next(err); //pass error
+});
+
+app.use((req, res, next) => {
+  console.log("This function has been ignored!");
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
