08a3b4e48e71b258ae90a53a997f0fd9792c8f15
Sardor Abdullaev <abdullaevss94@mail.ru>
2023-10-18 23:24:07 +0500

    refactor code

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/error.controller");
 const tourRouter = require("./routes/tour.router");
 const userRouter = require("./routes/user.router");
 
@@ -29,25 +31,18 @@ app.all("*", (req, res, next) => {
   //   message: `Can't find ${req.originalUrl} on this server!`,
   // });
 
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = "fail";
-  err.statusCode = 404;
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = "fail";
+  // err.statusCode = 404;
+  //next(new); //pass error
 
-  next(err); //pass error
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`));
 });
 
 app.use((req, res, next) => {
   console.log("This function has been ignored!");
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
