00161c78f6c8f6b2e850c62b59bb4f0acb0f2b5b
Sardor Abdullaev <abdullaevss94@mail.ru>
2023-10-22 16:13:26 +0500

    add uncaught and unhandled rejections

--- server.js
+++ server.js
@@ -11,3 +11,19 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('uncaughtException',err=>{
+   console.log("UNHANDLED EXCEPTION! 💥 Shutting down...");
+   console.log(err.name,err.message);
+   server.close(() => {
+     process.exit(1);
+   });
+})
