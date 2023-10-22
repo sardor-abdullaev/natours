bdf74f4d98ff61cd490a7fb1d0e82978f0e7bee6
Sardor Abdullaev <abdullaevss94@mail.ru>
2023-10-22 17:08:16 +0500

    add user model

---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: [true, "Please, enter your name!"] },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide valid email"],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
