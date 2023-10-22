d91d10b877f88dee501b6f0d816d8b647de301a0
Sardor Abdullaev <abdullaevss94@mail.ru>
2023-10-22 19:58:02 +0500

    add signup

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const userController = require('./../controllers/user.controller');
+const express = require("express");
+const userController = require("./../controllers/user.controller");
+const authController = require("./../controllers/auth.controller");
 
 const router = express.Router();
 
+router.post("/signup", authController.signup);
+
 router
-  .route('/')
+  .route("/")
   .get(userController.getAllUsers)
   .post(userController.createUser);
 
 router
-  .route('/:id')
+  .route("/:id")
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
