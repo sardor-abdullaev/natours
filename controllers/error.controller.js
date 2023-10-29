6a988ee6d8026f592ca373c41bcd85dc18775a32
Sardor Abdullaev <abdullaevss94@mail.ru>
2023-10-29 21:31:13 +0500

    bugfix

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -41,21 +41,16 @@ const sendErrorProd = (err, res) => {
 };
 
 module.exports = (err, req, res, next) => {
-  //   console.log(err.stack);
-  // console.log(process.env);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
   if (process.env.NODE_ENV === "development") {
-    let error = { ...err };
-    console.log(error.message);
-    if (error.name === "JsonWebTokenError") error = handleJWTError();
-    if (error.name === "TokenExpiredError") error = handleJWTExpiresError();
-    sendErrorDev(error, res);
+    sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
     let error = { ...err };
     if (error.name === "CastError") error = handleCastErrorDB(error);
-
+    if (error.name === "JsonWebTokenError") error = handleJWTError();
+    if (error.name === "TokenExpiredError") error = handleJWTExpiresError();
     sendErrorProd(error, res);
   }
 };
